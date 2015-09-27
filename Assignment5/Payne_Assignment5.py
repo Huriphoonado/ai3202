c209f6d7e958f9f1d81c5f9a6fb06a826f67c247
Willie Payne <williepain@gmail.com>
2015-09-27 14:38:19 -0600

    Implemented a calculate reward function and wrote a few black-box tests that appear to be working as expected

--- Assignment5/Payne_Assignment5.py
+++ Assignment5/Payne_Assignment5.py
@@ -16,18 +16,69 @@ import sys
 # transition model - successful .8 of the time, left .1 of the time, right .1 of the time
 
 # Node class containing all information needed for MDP
-# Initialized with an id, location, type, and weight all provided in World files
-# Nodes are stored in nodeDict, a dictionary representing our graph
+# Initialized with an location, type, and reward all provided in World files
+# Nodes are stored in nodeList, a dictionary representing our graph
 class Node(object):
 	def __init__(self, location, type, reward):
 		self.location = location # [x,y]
 		self.type = type # 0 = path, 1 = mountain, 2 = wall, 3 = Snake, 4 = barn, 5 = terminal
 		self.reward = reward # Determined by Node Type
 		self.utility = 0
+		self.direction = '' # calulated by whichever direction yields the highest utility
+		self.parent = None # Pointer to the Node of whichever direction yields the highest utility
+		# Adjacent nodes are set after the world has been read in
 		self.north = None
 		self.south = None
 		self.east = None
 		self.west = None
+
+# Challenges I'm thinking about
+# 	What happens if parent node is an intentional bounce back to avoid negative points
+# 		Issue only in determing final path from what the directions are asking for
+# 	Confusion between rewards and utilities - should utilities be initialized to the reward?
+# 		May only affect initial iteration
+# 	How should I iterate?
+# 		If I iterate through all nodes, then 0-nodes may not change causing the loop to end early
+# 		Maybe could be changed with a state parameter
+
+	# Calculates the utility of the current node
+	# 	If one of the adjacent nodes is None - we bounce back and use the utility of the current
+	# 	node in our calculations
+	def calculateUtility(self):
+		y = 0.9 # discount factor
+
+		# Determine what values to use in the calculation
+		if self.north != None:
+			northUtil = self.north.utility
+		else:
+			northUtil = self.utility
+		if self.south != None:
+			southUtil = self.south.utility
+		else:
+			southUtil = self.utility
+		if self.east != None:
+			eastUtil = self.east.utility
+		else:
+			eastUtil = self.utility
+		if self.west != None:
+			westUtil = self.west.utility
+		else:
+			westUtil = self.utility
+
+		northOption = (0.8 * northUtil) + (0.1 * eastUtil) + (0.1 * westUtil)
+		southOption = (0.8 * southUtil) + (0.1 * eastUtil) + (0.1 * westUtil)
+		eastOption = (0.8 * eastUtil) + (0.1 * northUtil) + (0.1 * southUtil)
+		westOption = (0.8 * westUtil) + (0.1 * northUtil) + (0.1 * southUtil)
+
+		options = ([northOption, 'North', self.north], [southOption, 'South', self.south], 
+			[eastOption, 'East', self.east], [westOption, 'West', self.west])
+		bestOption = max(options)
+		
+		self.utility = self.reward + (y * bestOption[0])
+		self.direction = bestOption[1]
+		self.parent = bestOption[2]
+
+		return self.utility
 
 # Read in the map file to use and e as commandline arguments
 # If invalid or not supplied - return False
@@ -111,9 +162,6 @@ def calcAdjNodes(nodeList):
 				current.south = nodeList[x][y-1]
 	return nodeList
 
-	
-
-
 if __name__ == "__main__":
 	mapFile, eVal = inputText()
 	if mapFile != False and eVal != False:
@@ -121,11 +169,11 @@ if __name__ == "__main__":
 		mapString = readFile(mapFile)
 		nodeList = stringToList(mapString)
 		nodeList = calcAdjNodes(nodeList)
-		print nodeList[1][0].north.type
-		print nodeList[1][0].south
-		print nodeList[1][0].east.type
-		print nodeList[1][0].west.type
-		#for y in range(7, -1, -1):
-			#for x in range(10):
-				#print mapList[x][y].type,
-			#print ''
+		print nodeList[9][7].calculateUtility()
+		print nodeList[8][7].calculateUtility()
+		print nodeList[8][7].direction
+		print nodeList[7][7].calculateUtility()
+		for y in range(7, -1, -1):
+			for x in range(10):
+				print mapList[x][y].utility,
+			print ''
