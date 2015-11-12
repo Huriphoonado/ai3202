54826179c21e39cf62a4ff18bad69f4690656f7d
Willie Payne <williepain@gmail.com>
2015-11-11 18:43:31 -0700

    Completed Part 1

--- Assignment8/Payne_Assignment8.py
+++ Assignment8/Payne_Assignment8.py
@@ -3,18 +3,23 @@
 
 import sys
 
-# Create a dict with actual letter and then counts for how many times the actual
-# letter appears and how many times the incorrect letters appear.
-# Then go back through and calculate the probabilities and store them within each dict
-# 	I think that these are the emission probabilities
-
+# X[t] representing letter states
 class Letter(object):
 	def __init__(self, letter):
 		self.letter = letter # letter is the actual state letter
+		self.occurences = 0. # count of times that the letter state appears in the data
+		self.initialState = 0. # Amount of times letter appears divided by total letters
+		
 		self.observed = {} # dict containing counts of actual letters typed
 		self.emmissions = {} # dict containing emmission probabilities
+
+		# Initialize these to contain all of the letters
 		self.nextLetters = {} # dict containing counts of letters observed after current letter
 		self.transitions = {} # dict containing transition probabilities
+
+		for l in range(97,123):
+			self.nextLetters[chr(l)] = 0.
+		self.nextLetters["_"] = 0.
 
 	# Once we observe a letter, add it to the observed or nextLetters dict or add to its count
 	def increment(self, observedLetter, correctDict):
@@ -24,6 +29,7 @@ class Letter(object):
 			correctDict[observedLetter] = 1.
 
 	# Update the emisstions dict to contain probabilities for all observed letters
+	# Emissions Probabilities only have observed values local to them
 	def calcEmissions(self):
 		statesNum = len(self.observed) # possible states
 		totalStates =  0. # Count of all observed states
@@ -37,16 +43,19 @@ class Letter(object):
 			self.emmissions[observedLetter] = (1 + self.observed[observedLetter])/(statesNum + totalStates)
 
 	# Update the transitions dict to contain probabilities for all next letters
+	# With transitions - there is the chance for any letter, though it some are very very small
 	def calcTransitions(self):
-		statesNum = len(self.observed)
+		statesNum = len(self.nextLetters) # Should be 27
 		totalStates =  0.
 
 		for nextLetter in self.nextLetters:
 			totalStates = totalStates + self.nextLetters[nextLetter]
 
 		for nextLetter in self.nextLetters:
-			print nextLetter
 			self.transitions[nextLetter] = (1 + self.nextLetters[nextLetter])/(statesNum + totalStates)
+
+	def addToCount(self):
+		self.occurences = self.occurences + 1.
 
 # Read in the data file and return a list of all the outputs
 # 	return letterList -> letterList[position][0 = state | 1 = observed]
@@ -76,6 +85,8 @@ def createLetters():
 	letters["_"] = newLetter
 	return letters
 
+# -------------------------------------------------------------
+
 # Iterate through all of the text file
 # 	With each state - count the number of observations
 def countObservations(letters, letterList):
@@ -92,6 +103,16 @@ def countTransitions(letters, letterList):
 		letters[state].increment(nextState, letters[state].nextLetters)
 	return letters
 
+# Iterate through all of the text file and count the number of times each
+# letter appears - Used for initial state distribution
+def countOccurences(letters, letterList):
+	for i in letterList:
+		state = i[0]
+		letters[state].addToCount()
+	return letters
+
+# -------------------------------------------------------------
+
 # Iterates through all of the letters and calculates emmissions
 def calcAllEmissions(letters):
 	for l in letters:
@@ -105,20 +126,40 @@ def calcAllTransitions(letters):
 
 	return letters
 
+def calcAllInitialStateDist(letters):
+	total = 0.
+	for i in letters:
+		total = total + letters[i].occurences
+	for i in letters:
+		letters[i].initialState = letters[i].occurences/total
+
+	return letters
+
+# -------------------------------------------------------------
+
 # Simply Iterates through all of the letters and prints emmissions tables
 def printAllEmissions(letters):
 	for l in letters:
 		print "State %s: Observations" % l,
 		for i in letters[l].emmissions:
-			print "(%s: %.3f)" % (i, letters[l].emmissions[i]),
+			print "(%s|%s: %.3f)" % (i, l, letters[l].emmissions[i]),
 		print ""
 
 def printAllTransitions(letters):
 	for l in letters:
 		print "State %s: Transitions" % l,
 		for i in letters[l].transitions:
-			print "(%s: %.3f)" % (i, letters[l].transitions[i]),
+			print "(%s|%s: %.3f)" % (i, l, letters[l].transitions[i]),
 		print ""
+
+def printAllInitStateDist(letters):
+	total = 0.
+	for l in letters:
+		total = total + letters[l].initialState
+		print "State %s: Initial State Distribution %.5f" % (l, letters[l].initialState)
+	print "Total: %.5f" % total
+
+# -------------------------------------------------------------
 
 def main():
 	letterList = readFile()
@@ -130,11 +171,15 @@ def main():
 	letters = calcAllEmissions(letters)
 	letters = countTransitions(letters, letterList)
 	letters = calcAllTransitions(letters)
+	letters = countOccurences(letters, letterList)
+	letters = calcAllInitialStateDist(letters)
 
 	print "-----------------Emissions Probabilities-----------------"
 	printAllEmissions(letters)
 	print "-----------------Transition Probabilities-----------------"
 	printAllTransitions(letters)
+	print "----------------Initial State Distribution----------------"
+	printAllInitStateDist(letters)
 
 if __name__ == '__main__':
 	main()
